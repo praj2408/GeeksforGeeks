6e18e242d3312da851f7239e6c4af974eee9abaf
Prajwal Krishna <prajwalgbdr03@gmail.com>
2026-05-28 09:08:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySum(self, arr):
+        # code here
+        
+        
+        summation = 0
+        
+        for num in arr:
+            summation = summation + num
+            
+        return summation
