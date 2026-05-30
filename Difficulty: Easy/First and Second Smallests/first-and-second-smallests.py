5de2cc57dac3e6880a0cd4ceda3b08f6df66dcf8
Prajwal Krishna <prajwalgbdr03@gmail.com>
2026-05-30 11:53:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First and Second Smallests/first-and-second-smallests.py
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.py
@@ -5,14 +5,17 @@ class Solution:
         smallest = float('inf')
         sec_smallest = float('inf')
         
+        
         for num in arr:
             if num < smallest:
                 sec_smallest = smallest
                 smallest = num
+                
             elif num < sec_smallest and num != smallest:
                 sec_smallest = num
                 
         if sec_smallest == float('inf'):
             return [-1]
+                
         
         return [smallest, sec_smallest]
