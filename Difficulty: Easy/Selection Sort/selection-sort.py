152aabaff630e5a41cd423f8b5e40d50ee70bf00
Prajwal Krishna <prajwalgbdr03@gmail.com>
2026-06-05 14:02:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -0,0 +1,15 @@
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        
+        n = len(arr)
+        for i in range(0, n):
+            min_index = i
+            
+            for j in range(i+1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+                    
+            arr[i], arr[min_index]= arr[min_index], arr[i]
+            
+        return arr
