2d64a991ee6840ba5c15dd3105965b44515e0509
Prajwal Krishna <prajwalgbdr03@gmail.com>
2026-06-12 19:04:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+# Design a class as described in the question
+# Code here
+
+# The call to display function is made like this
+# obj = MyClass()
+# obj.display()
+
+class MyClass:
+    def display(self):
+        print("Hello World")
+        
+
+# obj = MyClass()
+# obj.display()
