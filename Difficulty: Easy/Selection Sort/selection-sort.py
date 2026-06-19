baf0e430593a22c140f6135b1be80e1036b3fcfb
Prajwal Krishna <prajwalgbdr03@gmail.com>
2026-06-19 23:58:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.py
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -3,13 +3,16 @@ class Solution:
         #code here
         
         n = len(arr)
+        
         for i in range(0, n):
+            
             min_index = i
             
             for j in range(i+1, n):
+                
                 if arr[j] < arr[min_index]:
                     min_index = j
                     
-            arr[i], arr[min_index]= arr[min_index], arr[i]
+            arr[i], arr[min_index] = arr[min_index], arr[i]
             
         return arr
